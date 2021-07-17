261c70d11c3336861a1dfa0baf87a06f5c1a7b57
Emile Thigpen <emilethigpen@gmail.com>
2021-07-16 21:12:47 -0500

    modularized

---
+++ flask_app/controllers/users.py
@@ -0,0 +1,55 @@
+from flask_app import app
+from flask import render_template, redirect, request, session
+
+from flask_app.models.users import User
+
+
+@app.route("/users")
+def index():
+    users = User.get_all_users()
+    return render_template("index.html", users = users)
+
+@app.route("/users/new")
+def create_page():
+    
+    return render_template("create.html")
+
+@app.route("/users/create", methods=["POST"])
+def create():
+    User.create_user(request.form)
+    return redirect("/users")
+
+@app.route("/users/<int:id>/delete")
+def delete(id):
+    data = {
+        'id': id
+    }
+    User.delete_user(data)
+    return redirect("/users")
+
+@app.route("/users/<int:id>")
+def show(id):
+    data = {
+        'id': id
+    }
+    user = User.get_user(data)
+    return render_template("show.html", user = user)
+
+@app.route("/users/<int:id>/edit")
+def edit_user(id):
+    data = {
+        'id': id
+    }
+    user = User.get_user(data)
+    return render_template("edit.html", user = user)
+
+@app.route("/users/<int:id>/update", methods = ["POST"])
+def update_user(id):
+    data = {
+        'id': id,
+        'first_name': request.form['first_name'],
+        'last_name': request.form['last_name'],
+        'email': request.form['email']
+    }
+    User.update_user(data)
+    return redirect("/users")
